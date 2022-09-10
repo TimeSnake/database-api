d798bda47a95b314194ed3c3ef695dda5fe72cdf
funzter <funzter@mail.timesnake.de>
2022-09-10 20:47:06 +0200

    updated non tmp game servers

--- src/main/java/de/timesnake/database/core/server/DbNonTmpGameServer.java
+++ src/main/java/de/timesnake/database/core/server/DbNonTmpGameServer.java
@@ -4,6 +4,8 @@ import de.timesnake.database.core.Column;
 import de.timesnake.database.util.object.DatabaseConnector;
 import de.timesnake.database.util.object.Type;
 
+import java.util.UUID;
+
 public class DbNonTmpGameServer extends DbPvPServer implements de.timesnake.database.util.server.DbNonTmpGameServer {
 
     public DbNonTmpGameServer(DatabaseConnector databaseConnector, Integer port, String nameTable) {
@@ -18,6 +20,21 @@ public class DbNonTmpGameServer extends DbPvPServer implements de.timesnake.data
     @Override
     public void setGameInfo(String info) {
         super.setWithKey(info, Column.Server.GAME_INFO);
+    }
+
+    @Override
+    public UUID getOwnerUuid() {
+        return super.getFirstWithKey(Column.Server.OWNER);
+    }
+
+    @Override
+    public void setOwnerUuid(UUID uuid) {
+        super.setWithKey(uuid, Column.Server.OWNER);
+    }
+
+    @Override
+    public boolean hasOwner() {
+        return this.getOwnerUuid() != null;
     }
 
     @Override
--- src/main/java/de/timesnake/database/util/server/DbNonTmpGameServer.java
+++ src/main/java/de/timesnake/database/util/server/DbNonTmpGameServer.java
@@ -1,8 +1,16 @@
 package de.timesnake.database.util.server;
 
+import java.util.UUID;
+
 public interface DbNonTmpGameServer extends DbTaskServer {
 
     String getGameInfo();
 
     void setGameInfo(String info);
+
+    UUID getOwnerUuid();
+
+    void setOwnerUuid(UUID uuid);
+
+    boolean hasOwner();
 }
